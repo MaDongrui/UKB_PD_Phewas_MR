f189ef3003a9e065319080fe092bdf65f1bddb48
DongruiMa <madongrui0420@163.com>
2024-06-02 20:53:58 +0800

    Update PHESANT.sh

--- PHESANT.sh
+++ PHESANT.sh
@@ -13,9 +13,10 @@ df_merged.to_csv('ukbdata.csv', index=False)
 
 exit()
 
+# The traitofinterestfile points to different StandardPRS_PD_pT[1/0.5/0.1/0.05/0.01/0.005/0.001/0.0005].csv files.
 parallel -j 20 'Rscript phenomeScan.r \
 --phenofile="/home/mdr/PHESANT/WAS/data/ukbdata.csv" \
---traitofinterestfile="/home/mdr/PHESANT/WAS/data/StandardPRS_PD.csv" \
+--traitofinterestfile="/home/mdr/PHESANT/WAS/data/StandardPRS_PD_pT1.csv" \
 --variablelistfile="/home/mdr/PHESANT/variable-info/outcome-info.tsv" \
 --datacodingfile="/home/mdr/PHESANT/variable-info/data-coding-ordinal-info.txt" \
 --traitofinterest="exposure" \
