1d560c7d24de0a085c98a34b9bf715fe62770ca6
DongruiMa <madongrui0420@163.com>
2024-06-03 15:04:23 +0800

    Create PRSice.sh
    Calculation of polygenic risk score

---
+++ PRSice.sh
@@ -0,0 +1,24 @@
+./PRSice_linux \
+    --a1 A1 \
+    --a2 A2 \
+    --all-score  \
+    --bar-levels 0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1 \
+    --base /home/server/PRSice2/PRSice/PDMeta/PDMeta_CHR_OR_Sumstats.txt \
+    --binary-target T \
+    --chr CHR \
+    --clump-kb 250kb \
+    --clump-p 1.000000 \
+    --clump-r2 0.100000 \
+    --fastscore  \
+    --num-auto 22 \
+    --or  \
+    --out /home/server/PRSice2/PRSice/PDMeta/results_merge/merge1_22 \
+    --pheno /home/server/PRSice2/PRSice/PDMeta/FID_IID_PDphe.txt \
+    --pheno-col PD_phe \
+    --pvalue P \
+    --snp SNP \
+    --stat OR \
+    --target /media/server/data3/Genotype_calls/Genotype_calls/merge/merged1_22 \
+    --thread 60
+
+    
